3de0e81bcfe2c271f390253db80b7f2837232718
Maridmi <yarova0marina@gmail.com>
2024-03-31 16:33:27 +0300

    index updated

--- js/script.js
+++ js/script.js
@@ -16,3 +16,23 @@ $('#lang-switch').change(function () { // put onchange event when user select op
             $('[lang="ru"]').show();
         }
 });
+
+// -----------------------------------------
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
